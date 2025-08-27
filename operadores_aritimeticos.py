e6481d9c5eb8526e8bc4b45ecd0cd81f7781432d
Bruno Mendonça <103976296+brunomendoncadev@users.noreply.github.com>
2025-08-27 17:44:46 -0300

    operadores aritimeticos

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,35 @@
+Produto_1 = 20
+Produto_2 = 10
+
+print(Produto_1 + Produto_2 + 3.5)
+print(Produto_1 - Produto_2)
+print(Produto_1 / Produto_2)
+print(Produto_1 // Produto_2)
+print(Produto_1 * Produto_2)
+print(Produto_1 % Produto_2)
+print(Produto_1 ** Produto_2)
+
+x = 10 + 5 * 4
+print(x)
+
+y = (10 + 5) * 4
+print(y)
+
+# sequencia da conta abaixo é:
+# 1° 2 ** 2 = 4
+# 2° 25 * 2 = 50
+# 3° 10 / 2 = 5
+# 4° 5 + 50 = 55 - 4 = 51
+
+z = 10 / 2 + 25 * 2 - 2 ** 2
+print(z)
+
+# sequencia da conta abaixo é:
+# 1° ( 2 - 2) = 0
+# 2° ( 0 ** 2 ) = 0
+# 3°  25 * 0 = 0
+# 4°  (10/ 2) = 5 
+# 5° 5 + 0 = 5.0
+
+t = (10 / 2) + 25 *(( 2 - 2) ** 2)
+print(t)
