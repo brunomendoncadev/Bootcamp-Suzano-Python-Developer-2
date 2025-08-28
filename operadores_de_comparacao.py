4b12bb7c06a964122b94cff899a50109e9056cbe
Bruno Mendonça <103976296+brunomendoncadev@users.noreply.github.com>
2025-08-27 23:02:13 -0300

    operadores de comparacao

---
+++ operadores_de_comparacao.py
@@ -0,0 +1,22 @@
+saldo = 450
+saque = 200
+
+print(saldo == saque)
+#na linha acima esta comparando se saldo é igual a saque
+#false
+
+print( saldo != saque)
+#na linha acima esta comparando se saldo é diferente de saque
+#True
+
+print(saldo > saque)
+#na linha acima esta comparando se saldo é maior que saque
+#True
+
+print(saldo >= saque)
+#na linha acima esta comparando se saldo é maior ou igual a saque
+#True
+
+print (saldo <= saque)
+#na linha acima esta comparando se saldo é menor ou igual a saque
+#False
