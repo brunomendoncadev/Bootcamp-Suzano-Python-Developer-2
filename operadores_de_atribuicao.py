e3ff0459edd2a87d26dfda0c65a7b228b43beace
Bruno Mendonça <103976296+brunomendoncadev@users.noreply.github.com>
2025-08-27 23:42:32 -0300

    operadores de atribuicao

---
+++ operadores_de_atribuicao.py
@@ -0,0 +1,59 @@
+
+# Atribuição simples
+saldo = 500
+#  saldo é uma variavel 
+#  = é uma atribuição simples
+#  500 é o valor
+print(saldo)
+# >>>500
+
+
+# Atribuição Adição
+saldo1 = 500
+saldo1 += 200
+print(saldo1)
+# >>>700
+
+
+# Atribuição com Subtração
+saldo2 = 500
+saldo2 -= 100
+print(saldo2)
+# >>>400
+
+# Atribuição com Multiplicação
+saldo3 = 500
+saldo3 *= 2 
+print(saldo3)
+# >>>1000
+
+# Atribuição com Divisão
+saldo4 = 500
+saldo4 /= 5  # divisao com ponto flutuante com /
+print(saldo4)
+# >>>100.0
+
+# Atribuição com Divisão
+saldo5 = 500
+saldo5 //= 5  #divisao inteira com // 
+print(saldo5)
+# >>>100
+
+# atribuição com Módulo
+saldo6 = 500
+saldo6 %= 480  #resto da divisao entre 500 e 480
+print(saldo6)
+# >>>20
+
+# Atribuição com esponenciação
+saldo7 = 80
+saldo7 **= 2
+print(saldo7)
+# >>>6400
+
+
+
+
+
+
+
